5f8a16916dab252fdd4330fb1747631363409daa
Jonathan Montgomery <jonathan.montgo@gmail.com>
2021-11-19 16:24:26 -0600

    Change name to be bandcamp_extract

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BandCamp Extractor',
+    name='bandcamp_extract',
     version='0.1.0',
     entry_points={
         'console_scripts': [
