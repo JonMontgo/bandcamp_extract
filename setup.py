19bf0b73c9b9d5984694146c39ecf256b0a1028b
Jonathan Montgomery <jonathan.montgo@gmail.com>
2021-11-19 16:27:18 -0600

    Better setup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='bandcamp_extract',
     version='0.1.0',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
     entry_points={
         'console_scripts': [
             'bcextr = src.extract:extract'
@@ -13,6 +19,8 @@ setup(
     python_requires=">=3.6",
     description=('Extract BandCamp .zip album folders to their appropriate '
                  'place'),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Jonathan Montgomery',
     author_email='jonathan.montgo@gmail.com',
     url='https://github.com/JonMontgo/bandcamp_extract',
